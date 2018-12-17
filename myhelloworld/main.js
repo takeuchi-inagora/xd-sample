a17887bf47e2f7065787e695d4cdf4524d9d8eb3
tomoka takeuchi <takeuchi@inagora.com>
2018-12-17 21:39:17 +0900

    パーツだけ揃えた

--- myhelloworld/main.js
+++ myhelloworld/main.js
@@ -118,15 +118,22 @@ function helloHandlerFunction(selection) { // メインのファンクション
 
 async function showAlert() {
     /* we'll display a dialog here */
-    console.log("エラー");
+    console.log("アラート");
+    await alert("Connect to the Internet", //[1]
+    "In order to function correctly, this plugin requires access to the Internet. Please connect to a network that has Internet access."); //[2]
 }
 
-await error("Synchronization Failed", //[1]
+async function showError() {
+    /* we'll display a dialog here */
+    console.log("エラー");
+    await error("Synchronization Failed", //[1]
     "Failed to synchronize all your changes with our server. Some changes may have been lost.",
     "Steps you can take:",
     "* Save your document",
     "* Check your network connection",
     "* Try again in a few minutes"); //[2]
+
+}
 
 module.exports = { // コマンドIDとファンクションの紐付け
   commands: {
